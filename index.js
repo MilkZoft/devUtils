79f1ea7b5765d4ab630c5a353134eaf31ab984fa
Carlos Santana <carlos.x.santana.-nd@disney.com>
2015-02-07 18:32:13 -0800

    First Commit

---
+++ index.js
@@ -0,0 +1,75 @@
+"use strict";
+
+module.exports = {
+  isDesktop: function(ua) {
+    return !(/mobile/i.test(ua));
+  },
+
+  isMobile: function(ua) {
+    return (/mobile/i.test(ua));
+  },
+
+  getCurrentDevice: function(ua) {
+    return (/mobile/i.test(ua)) ? 'mobile' : 'desktop';
+  },
+
+  isYear: function(year) {
+    return (typeof(year) !== "undefined" && year.length === 4 && !isNaN(year));
+  },
+
+  isMonth: function(month) {
+    return (typeof(month) !== "undefined" && month.length === 2 && !isNaN(month) && month <= 12);
+  },
+
+  isDay: function(day) {
+    return (typeof(day) !== "undefined" && day.length === 2 && !isNaN(day) && day <= 31);
+  },
+
+  isDefined: function(variable) {
+    return (typeof(variable) !== "undefined") ? true : false;
+  },
+
+  getParamsFromUrl: function(params) {
+    params = params.split('/');
+    params.shift();
+
+    return params;
+  },
+
+  randomCode: function(max, charSet) {
+    max = max || 12;
+    charSet = charSet || 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    var randomCode = '';
+    var randomPoz;
+
+    for (var i = 0; i < max; i++) {
+      randomPoz = Math.floor(Math.random() * charSet.length);
+      randomCode += charSet.substring(randomPoz, randomPoz + 1);
+    }
+
+    return randomCode;
+  },
+
+  removeHTML: function(str) {
+    return str.replace(/(<([^>]+)>)/ig, "");
+  },
+
+  escape: function(html) {
+    return String(html)
+      .replace(/&/g, '&amp;')
+      .replace(/"/g, '&quot;')
+      .replace(/'/g, '&#39;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;');
+  },
+
+  unescape: function(html) {
+    return String(html)
+      .replace(/&amp;/g, '&')
+      .replace(/&quot;/g, '"')
+      .replace(/&#39;/g, "'")
+      .replace(/&lt;/g, '<')
+      .replace(/&gt;/g, '>');
+  }
+};
